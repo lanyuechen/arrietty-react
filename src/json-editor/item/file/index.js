bcc86b33f2471c8990aa97bc5c83fe06e5070852
zhenglipeng <1252012805@qq.com>
2019-03-14 13:28:43 +0800

    上传文件表单完善

--- src/json-editor/item/file/index.js
+++ src/json-editor/item/file/index.js
@@ -8,8 +8,28 @@ export default class extends Component {
   }
 
   handleChange = (e) => {
-    const { onChange } = this.props;
-    onChange && onChange(e.target.value);
+    const { onChange, describe } = this.props;
+    const { url, name = 'bin' } = describe.config;
+
+    console.log('==', name)
+    
+    const file = e.target.files[0];
+    const body = new FormData();
+    body.append(name, file);
+
+    console.log('describe', describe)
+    fetch(url, {
+      method: 'POST',
+      body,
+      // 注！加headers会导致上传失败，原因未知
+      // headers: {
+      //   'Content-Type': 'multipart/form-data'
+      // }
+    }).then(res => res.json()).then(res => {
+      if (res.code === 200) {
+        onChange && onChange(res.msg);
+      }
+    });
   }
 
   render() {
@@ -17,7 +37,11 @@ export default class extends Component {
     console.log('------', name, value, describe)
     return (
       <div className="ar-img-upload">
-        IMG
+        {value && value._id ? (
+          <img src={`http://localhost:3001/upload/${value._id}.${value.ext}`} alt="IMG"/>
+        ) : (
+          'IMG'
+        )}
         <input type="file" onChange={this.handleChange} />
       </div>
     )
