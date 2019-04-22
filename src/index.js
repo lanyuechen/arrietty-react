d172aa11580ac10dff3bf6317306abb9c6b2bc32
zhenglipeng <1252012805@qq.com>
2019-04-22 15:03:54 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ export Aio from './all-in-one';
 export AspectRatioBox from './aspect-ratio-box';
 export JsonEditor from './json-editor';
 export CodeEditor from './code-editor';
+export Menu from './menu';
 export dnd from './dnd';
